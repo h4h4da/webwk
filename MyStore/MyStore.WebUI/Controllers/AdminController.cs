a217cf4e58fc68985f69623e55dc270924ae2f64
h4h4da <1436411225@qq.com>
2016-11-04 19:34:44 +0800

    11-4提交

---
+++ MyStore/MyStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using MyStore.Domain.Abstract;
+using MyStore.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStore.WebUI.Controllers
+{
+  
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+        public AdminController(IProductsRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products.ToList());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.Id == id);
+            IEnumerable<SelectListItem> selectListItem = 
+                repository.Categories.ToList().Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name });
+            ViewBag.CategoryList = selectListItem;
+            return View(product);
+        }
+
+
+    }
+}
